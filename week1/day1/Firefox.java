4a263ffb0552a1466c3114acc4c7564aa935fb0e
vinodh-raja <119944109+vinodh-raja@users.noreply.github.com>
2023-07-18 00:39:55 +0530

    Add files via upload
    Week1

---
+++ week1/day1/Firefox.java
@@ -0,0 +1,19 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+
+		String name="Firefox";
+		float version=100.2f;
+		boolean isVisible=true;
+		int year=1998;
+		char logo='M';
+		
+		System.out.println("Name of browser : "+name);
+		System.out.println("Version : "+version +"[" +isVisible +"]");
+		System.out.println("Released in : "+year);
+		System.out.println("Logo is : "+logo);
+	}
+
+}
