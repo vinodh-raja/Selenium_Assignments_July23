a20b4a76571a608e9988f8f20f7c817c8920ff9f
vinodh-raja <119944109+vinodh-raja@users.noreply.github.com>
2023-07-18 23:48:12 +0530

    Add files via upload

---
+++ day2/ConvertANegativeNumberToPositiveNumber.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class ConvertANegativeNumberToPositiveNumber {
+
+	public static void main(String[] args) {
+
+		int number=-40;
+		if(number<0) {
+			int num=(-40)*(-1);
+			System.out.println("The negative number"+number+ "is converted to Positive number "+num);
+		}else{
+			
+			System.out.println("The number is Positive number"+number);
+		}
+		
+	}
+	
+}
+	
+	
+	
