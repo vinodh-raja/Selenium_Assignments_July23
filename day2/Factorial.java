e7aaf135d08c4a387eac6bfb141318b7f21b7e0b
vinodh-raja <119944109+vinodh-raja@users.noreply.github.com>
2023-07-21 19:17:55 +0530

    Add files via upload

---
+++ day2/Factorial.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+
+		int fact=1;
+		int n=5;
+		for(int i=1;i<=n;i++) {
+			fact=fact*i;
+		}
+		
+		System.out.println(fact);
+		
+	}
+
+}
